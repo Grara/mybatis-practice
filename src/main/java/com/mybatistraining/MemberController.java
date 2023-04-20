656c210b0590c9c05110fd5113874858b192128f
Grara <nomj18@gmail.com>
2023-04-20 23:36:47 +0900

    fetch 연습

--- src/main/java/com/mybatistraining/MemberController.java
+++ src/main/java/com/mybatistraining/MemberController.java
@@ -1,10 +1,15 @@
 package com.mybatistraining;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,5 +30,15 @@ public class MemberController {
     @GetMapping("/members3")
     public List<Member> findMembers3(){
         return memberDAO.findByname("노");
+    }
+
+    @GetMapping("/test")
+    public String test(){
+        return "test";
+    }
+
+    @PostMapping("/test2")
+    public ResponseEntity<Map<String, Object>> test2(@RequestBody Map<String, Object> map){
+        return new ResponseEntity<>(map, HttpStatus.BAD_REQUEST);
     }
 }
